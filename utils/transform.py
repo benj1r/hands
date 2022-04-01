c24e9ec15f3201b7785ec3b84f8b18d4f2a8bc3a
benj1r <47711426+benj1r@users.noreply.github.com>
2022-03-31 23:11:21 -0700

    coordinate transforms implemented

--- utils/transform.py
+++ utils/transform.py
@@ -5,15 +5,16 @@ def global2local(coord, R, T):
     Transforms world-space joint coordinate to camera-space joint coordinate
 
     Args:
-        coord: joint coordinate numpy array
+        coord: transpose joint coordinate numpy array
         R: camera rotation matrix
-        T: transpose camera position matrix
+        T: camera position matrix
     
     Returns:
         joint coordinate in camera-space
     """
-    pass
-
+    
+    coord = np.dot(R, coord - T)
+    return coord
 
 def local2pixel(coord, f, c):
     """
@@ -27,7 +28,16 @@ def local2pixel(coord, f, c):
     Returns:
         joint coordinate in pixel-space
     """
-    pass
+    
+    esp = 1e-8
+
+    x = coord[:, 0] / (coord[:, 2] + esp) * f[0] + c[0]
+    
+    y = coord[:, 1] / (coord[:, 2] + esp) * f[1] + c[1]
+    
+    z = coord[:, 2]
+    
+    return np.concatenate((x[:,None],y[:,None],z[:,None]),1)
 
 
 def pixel2local(coord, f, c):
@@ -42,6 +52,11 @@ def pixel2local(coord, f, c):
     Returns:
         joint coordinate in camera-space
     """
-    pass
-
+    x = (coord[:,0] - c[0]) / f[0] * coord[:,2]
+    
+    y = (coord[:,1] - c[1]) / f[1] * coord[:,2]
+    
+    z = coord[:, 2]
+    
+    return np.concatenate((x[:,None],y[:,None], z[:,None]),1)
 
