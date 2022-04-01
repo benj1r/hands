e2328711c682c814c2e1decb6cba18307022d5e1
benj1r <47711426+benj1r@users.noreply.github.com>
2022-03-31 23:12:13 -0700

    model config added

--- main/config.py
+++ main/config.py
@@ -2,6 +2,14 @@ import os.path as osp
 import sys
 
 class Config():
-    pass
+    root_dir = osp.join(osp.dirname(osp.abspath(__file__)),'..')
+    data_dir = osp.join(root_dir, 'data')
+    
+    height = 512
+    width = 512
 
 config = Config()
+
+sys.path.insert(0, osp.join(config.root_dir))
+from utils.dir import add_path
+add_path(config.data_dir)
---
+++ utils/dir.py
@@ -0,0 +1,8 @@
+import os
+import os.path as osp
+import sys
+
+def add_path(path):
+    if path not in sys.path:
+        sys.path.insert(0,path)
+
